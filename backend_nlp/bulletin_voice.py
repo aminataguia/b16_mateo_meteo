ccd9a156f71ac125eaf3a2550fa11d986ce453ae
aminataguia <amina.taguia.dev.ia@gmail.com>
2024-04-02 15:54:42 +0200

    maj
    correction le html renvoie bien un audio

--- backend_nlp/bulletin_voice.py
+++ backend_nlp/bulletin_voice.py
@@ -1,9 +1,9 @@
 import base64
 import httpx
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 import json
-
+from fastapi.responses import FileResponse
 from meteofrance_api import MeteoFranceClient
 from fonctions import get_forecast_for_city
 
@@ -22,10 +22,10 @@ app.add_middleware(
 headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMWI0MDFiNTQtNDA3ZS00YjliLWE2ZjQtODdhYzE2M2U0YTY3IiwidHlwZSI6ImFwaV90b2tlbiJ9.E4p5OS5QLYy2Tj7GTm-t9sWVsDA8UXUyKbHX1dUHE7U"
 }
+
 # Endpoint to generate weather bulletin
 @app.get("/weather-bulletin/{city}")
 async def weather_bulletin(city: str):
-
     url = "https://api.edenai.run/v2/text/generation"
     payload = {
         "providers": "openai,cohere",
@@ -34,12 +34,20 @@ async def weather_bulletin(city: str):
         "max_tokens": 250,
         "fallback_providers": ""
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, json=payload, headers=headers)
+    timeout = httpx.Timeout(10.0, read=None) # Ajustez le délai d'attente de lecture à None pour désactiver le délai d'attente de lecture
+    try:
+        async with httpx.AsyncClient(timeout=timeout) as client:
+            response = await client.post(url, json=payload, headers=headers)
+    except httpx.ConnectTimeout:
+        raise HTTPException(status_code=504, detail="Délai d'attente de connexion dépassé.")
+    except httpx.ReadTimeout:
+        raise HTTPException(status_code=504, detail="Délai d'attente de lecture dépassé.")
+    except httpx.WriteTimeout:
+        raise HTTPException(status_code=504, detail="Délai d'attente d'écriture dépassé.")
+
     if response.status_code == 200:
         result = response.json()
         generated_text = result['openai']['generated_text']
-        # return {"generated_text": generated_text}
 
         url_speech = "https://api.edenai.run/v2/audio/text_to_speech"
         payload_speech = {
@@ -49,17 +57,23 @@ async def weather_bulletin(city: str):
             "text": json.dumps(generated_text), # Convert the dictionary to a JSON string
             "fallback_providers": ""
         }
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url_speech, json=payload_speech, headers=headers)
+        try:
+            async with httpx.AsyncClient(timeout=timeout) as client:
+                response = await client.post(url_speech, json=payload_speech, headers=headers)
+        except httpx.ConnectTimeout:
+            raise HTTPException(status_code=504, detail="Délai d'attente de connexion dépassé.")
+        except httpx.ReadTimeout:
+            raise HTTPException(status_code=504, detail="Délai d'attente de lecture dépassé.")
+        except httpx.WriteTimeout:
+            raise HTTPException(status_code=504, detail="Délai d'attente d'écriture dépassé.")
+
         if response.status_code == 200:
             result = response.json()
             audio_data = result.get('google', {}).get('audio')
             if audio_data:
                 audio_bytes = base64.b64decode(audio_data)
-                # Save the audio file
-                with open("audio.mp3", "wb") as audio_file:
-                    audio_file.write(audio_bytes)
-                return {"message": "Fichier audio généré avec succès : audio.mp3"}
+                # Renvoyer le contenu du fichier audio directement
+                return Response(content=audio_bytes, media_type="audio/mpeg")
             else:
                 raise HTTPException(status_code=400, detail="Aucune donnée audio disponible dans la réponse.")
         else:
@@ -67,11 +81,6 @@ async def weather_bulletin(city: str):
     else:
         raise HTTPException(status_code=500, detail="Erreur lors de la requête à l'API de génération de texte.")
 
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8001)
-
-
-
-
--- front/script.js
+++ front/script.js
@@ -9,18 +9,18 @@ document.getElementById('weather-form').addEventListener('submit', async functio
     });
 
     if (response.ok) {
-        const data = await response.json();
-        if (data.message) {
-            alert(data.message);
-            // Assuming the audio file is saved locally and accessible
-            const audioElement = document.createElement('audio');
-            audioElement.src = "audio.mp3";
-            audioElement.controls = true;
-            document.getElementById('audio-container').appendChild(audioElement);
-        } else {
-            alert('Erreur lors de la génération du fichier audio.');
-        }
+        // Utilisez response.blob() pour traiter les données audio comme un blob
+        const audioBlob = await response.blob();
+        // Créez un objet URL pour le blob
+        const audioUrl = URL.createObjectURL(audioBlob);
+        // Créez un élément audio et définissez son source
+        const audioElement = document.createElement('audio');
+        audioElement.src = audioUrl;
+        audioElement.controls = true; // Ajoutez des contrôles pour jouer/pause
+        document.getElementById('audio-container').appendChild(audioElement); // Ajoutez l'élément audio au DOM
+        audioElement.play(); // Commencez à jouer l'audio
     } else {
         alert('Erreur lors de la requête à l\'API.');
     }
 });
+
